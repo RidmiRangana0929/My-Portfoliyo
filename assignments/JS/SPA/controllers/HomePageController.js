b39ac349c7aa3dc9df64ca2a67f07965b660de97
RidmiRangana0929 <ridmirangana1429@gmail.com>
2022-10-12 15:21:45 +0530

    content updated

--- assignments/JS/SPA/controllers/HomePageController.js
+++ assignments/JS/SPA/controllers/HomePageController.js
@@ -1,13 +1,27 @@
 $("#home").click(function () {
     $("#homeContent").css('display','block');
     $("#customerContent").css('display','none');
-    /*$("#itemContent").css('display','none');
-    $("#orderContent").css('display','none');*/
+    $("#itemContent").css('display','none');
+    $("#orderContent").css('display','none');
 });
 
 $("#customer").click(function () {
     $("#homeContent").css('display','none');
     $("#customerContent").css('display','block');
-    /*$("#itemContent").css('display','none');
-    $("#orderContent").css('display','none');*/
+    $("#itemContent").css('display','none');
+    $("#orderContent").css('display','none');
+});
+
+$("#item").click(function () {
+    $("#homeContent").css('display','none');
+    $("#customerContent").css('display','none');
+    $("#itemContent").css('display','block');
+    $("#orderContent").css('display','none');
+});
+
+$("#order").click(function () {
+    $("#homeContent").css('display','none');
+    $("#customerContent").css('display','none');
+    $("#itemContent").css('display','none');
+    $("#orderContent").css('display','block');
 });
