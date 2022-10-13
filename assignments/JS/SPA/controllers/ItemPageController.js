0e2350786e687055ea12e315ee0188ff511b624a
RidmiRangana0929 <ridmirangana1429@gmail.com>
2022-10-13 16:09:00 +0530

    content updated

--- assignments/JS/SPA/controllers/ItemPageController.js
+++ assignments/JS/SPA/controllers/ItemPageController.js
@@ -13,7 +13,35 @@ $("#btnItmAdd").click(function (){
     }
     items.push(itemObject);
 
-    loadAllCustomers();
+    loadAllItems();
 
-    bindRowClickEvents();
+    bindRowClickEvents2();
 });
+
+/*--------load all----------*/
+function loadAllItems(){
+
+    $("#tblItem").empty();
+
+    for (var item of items){
+        var row=`<tr><td>${item.code}</td><td>${item.name}</td><td>${item.qty}</td><td>${item.price}</td></tr>`;
+        $("#tblItem").append(row);
+    }
+
+}
+
+/*--------row click----------*/
+function bindRowClickEvents2() {
+    $("#tblItem>tr").click(function () {
+        let id = $(this).children(":eq(0)").text();
+        let name = $(this).children(":eq(1)").text();
+        let address = $(this).children(":eq(2)").text();
+        let salary = $(this).children(":eq(3)").text();
+
+        $('#ItemCode').val(id);
+        $('#ItemName').val(name);
+        $('#ItemQty').val(address);
+        $('#ItemPrice').val(salary);
+
+    });
+}
