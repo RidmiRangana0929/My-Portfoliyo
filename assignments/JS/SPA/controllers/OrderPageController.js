ecbe39f11370096c818ff30d136274cee318fbcd
RidmiRangana0929 <ridmirangana1429@gmail.com>
2022-10-14 20:18:25 +0530

    content updated

--- assignments/JS/SPA/controllers/OrderPageController.js
+++ assignments/JS/SPA/controllers/OrderPageController.js
@@ -11,3 +11,33 @@ function loadAllItemsForComboBox(){
         $("#selectItmCode").append(`<option>${it.code}</option>`)
     }
 }
+
+$("#addToCart").click(function (){
+    let itemCode=$("#selectItmCode").val();
+    let itemName=$("#selectedItmName").val();
+    let itemPrice=$("#selectedItmPrice").val();
+    let itemQty=$("#selectedQty").val();
+    let itemTotal=parseInt(itemPrice)*parseInt(itemQty);
+
+    var cartObject={
+        itCode: itemCode,
+        itName:itemName,
+        itPrice:itemPrice,
+        itQty:itemQty,
+        itTotal:itemTotal
+    }
+    cart.push(cartObject);
+
+    loadAllItemsToCart();
+});
+
+function loadAllItemsToCart(){
+
+    $("#tblCart").empty();
+
+    for (var c of cart){
+        var row=`<tr><td>${c.itCode}</td><td>${c.itName}</td><td>${c.itPrice}</td><td>${c.itQty}</td><td>${c.itTotal}</td></tr>`;
+        $("#tblCart").append(row);
+    }
+
+}
